30da230123bfb7eb429a8e5ea1bbd00ee76ed4aa
lielsh <liel_sharon@outlook.com>
2021-12-09 11:04:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
 
-export default App;
+    const [data, setData] = useState([]);
+
+    const getData = () => {
+        axios({
+            url: 'https://jsonplaceholder.typicode.com/users',
+            method: 'GET'
+        })
+        .then(res => res.data.filter(person => person.address.geo.lat > 0))
+        .then(data => setData(data))
+        .catch(err => console.log(err));
+    }
+
+    return (
+        <div className='App'>
+            <p>
+                <button onClick={getData}>
+                    Click Here!
+                </button>
+            </p>
+            <table style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+                <tbody>
+                    { data.length ?
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                        </tr>
+                        :
+                        null
+                    }
+                    {
+                        data.map((person, index) => 
+                            <tr key={index}>
+                                <td>{person.name.split(' ')[0]}</td>
+                                <td>{person.name.split(' ')[1]}</td>
+                            </tr>
+                        )
+                    } 
+                </tbody>
+            </table>
+        </div>
+    );
+}
